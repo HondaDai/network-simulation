2dfe12ec329c107dd79df659d76399348590037c
HondaDai <hondadai.tw@gmail.com>
2014-05-05 15:44:35 +0800

    parse csv

--- parse_csv.rb
+++ parse_csv.rb
@@ -7,10 +7,12 @@ client = CSVTable.read("Record_Client.csv")
 server = CSVTable.read("Record_Server.csv")
 
 
-
+puts "Direct Client: "
+puts "length: #{direct_client.size}"
 puts direct_client.avg_of("ElasedTime")
+
+puts "Client: "
+puts "length: #{client.size}"
 puts client.avg_of("ElasedTime")
-
-
 
 
